03fd639932d802268554de4ac450332866e3674e
Akmal Rian <akmalrian@gmail.com>
2026-05-19 23:01:20 +0700

    feat : add database.go backend

---
+++ config/database.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func ConnectDatabase() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Failed to open database:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = db
+	log.Println("Database connected!")
+}
